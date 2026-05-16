d377ab187b06966d6c9da2c7e87edfce4b91ed44
Stone-keep <radek12345@gmail.com>
2026-05-16 17:56:46 +0200

    Set up tool animations

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
 @onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/MoveStateMachine/playback")
+@onready var tool_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/ToolStateMachine/playback")
 
 const SPEED = 300.0
 var direction := Vector2.ZERO
 
 enum Tools {AXE, HOE, WATER_CAN}
 var current_tool := Tools.AXE
+const tool_connection = {
+	Tools.AXE: "axe",
+	Tools.HOE: "hoe",
+	Tools.WATER_CAN: "water"
+}
 
 func _physics_process(_delta: float) -> void:
 	get_input()
@@ -17,11 +23,14 @@ func _physics_process(_delta: float) -> void:
 
 func get_input() -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
-	if Input.is_action_just_pressed("tool_forward"):
-		current_tool = posmod(current_tool + 1, Tools.size()) as Tools
-		print(current_tool)
-	if Input.is_action_just_pressed("tool_backward"):
-		current_tool = posmod(current_tool - 1, Tools.size()) as Tools
+
+	if Input.is_action_just_pressed("action"):
+		tool_state_machine.travel(tool_connection[current_tool])
+		$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+
+	if Input.is_action_just_pressed("tool_backward") or Input.is_action_just_pressed("tool_forward"):
+		var tool_direction = Input.get_axis("tool_backward", "tool_forward") as int
+		current_tool = posmod(current_tool + tool_direction, Tools.size()) as Tools
 		print(current_tool)
 
 
@@ -31,5 +40,8 @@ func animation() -> void:
 		move_state_machine.travel("move")
 		$AnimationTree.set("parameters/MoveStateMachine/move/blend_position", target_vector)
 		$AnimationTree.set("parameters/MoveStateMachine/idle/blend_position", target_vector)
+		for state in tool_connection.values():
+			$AnimationTree.set("parameters/ToolStateMachine/%s/blend_position" % state, target_vector)
+
 	else:
 		move_state_machine.travel("idle")
