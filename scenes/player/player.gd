85c479de01c44445680981b844fa8e04d481b4e6
Stone-keep <radek12345@gmail.com>
2026-05-16 17:32:21 +0200

    Add tools enum and switching tools

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 var direction := Vector2.ZERO
 
+enum Tools {AXE, HOE, WATER_CAN}
+var current_tool := Tools.AXE
 
 func _physics_process(_delta: float) -> void:
 	get_input()
@@ -12,10 +14,18 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 	animation()
 
-func get_input():
-	direction = Input.get_vector("left", "right", "up", "down")
 
-func animation():
+func get_input() -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	if Input.is_action_just_pressed("tool_forward"):
+		current_tool = posmod(current_tool + 1, Tools.size()) as Tools
+		print(current_tool)
+	if Input.is_action_just_pressed("tool_backward"):
+		current_tool = posmod(current_tool - 1, Tools.size()) as Tools
+		print(current_tool)
+
+
+func animation() -> void:
 	if direction:
 		var target_vector: Vector2 = Vector2(round(direction.x), round(direction.y))
 		move_state_machine.travel("move")
